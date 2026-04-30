fb0d01251ed7f68cf60d80dfa56ee5c500151b77
omkarrathod123 <omkarrathod0705@gmail.com>
2026-04-30 17:56:55 +0530

    Adding Country Edit Page

---
+++ FindMyCountry.Client/Pages/CountryEdit.razor.cs
@@ -0,0 +1,64 @@
+﻿using FindMyCountry.shared;
+using Microsoft.AspNetCore.Components;
+using Microsoft.VisualBasic;
+using System.Net.Http.Json;
+
+namespace FindMyCountry.Client.Pages
+{
+    public partial class CountryEdit
+    {
+        [Parameter]
+        public int id { get; set; }
+        private string? error;
+        private Country? country = new Country();
+        protected override async Task OnInitializedAsync()
+        {
+            if(id == 0)
+            {
+                country = new Country();
+            }
+            else
+            {
+                country = await Http.GetFromJsonAsync<Country>("countryedit/" + id);
+            }
+        }
+        private async Task HandleValidSubmit()
+        {
+            HttpResponseMessage response;
+            if(country.CId == 0)
+            {
+                response = await Http.PostAsJsonAsync("countryedit", country);
+            }
+            else
+            {
+                string requestUri = "countries/" + country.CId;
+                response = await Http.PutAsJsonAsync(requestUri, country);
+            }
+            if (response.IsSuccessStatusCode)
+            {
+                navigationManager.NavigateTo("country");
+            }
+            else
+            {
+                error = response.ReasonPhrase;
+            }
+        }
+        private void HandleReset()
+        {
+            navigationManager.NavigateTo("country");
+        }
+        private async Task DeleteCountry()
+        {
+            string requestUri = "countries/" + country.CId;
+            var response = await Http.DeleteAsync(requestUri);
+            if (response.IsSuccessStatusCode)
+            {
+                navigationManager.NavigateTo("country");
+            }
+            else
+            {
+                error = response.ReasonPhrase;
+            }
+        }
+    }
+}
---
+++ FindMyCountry.Client/Pages/CountryPage.razor.cs
@@ -0,0 +1,14 @@
+﻿using FindMyCountry.shared;
+using System.Net.Http.Json;
+
+namespace FindMyCountry.Client.Pages
+{
+    public partial class CountryPage
+    {
+        IList<Country> countries;
+        protected override async Task OnInitializedAsync()
+        {
+            countries = await httpClient.GetFromJsonAsync<IList<Country>>("countries");
+        }
+    }
+}
